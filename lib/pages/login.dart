789d19197a067eecfe76a63f812f08e7ae580ea0
Momin Khatter <momin.khanzada1999@gmail.com>
2021-07-29 04:28:21 -0700

    animatated button is added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:fluttertraining/utils/routes.dart';
 
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+
+  bool changebutton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +27,7 @@ class LoginPage extends StatelessWidget {
               ),
               Image.asset('images/login.png',
               fit: BoxFit.fill,),
-              Text('Welcome',
+              Text('Welcome $name',
               style: TextStyle(
                 fontSize: 25,
                 fontWeight: FontWeight.bold,
@@ -35,6 +44,13 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter your Username",
                         labelText: "Username",
                       ),
+                      onChanged: (value)
+                      {
+                        name = value;
+                        setState(() {
+
+                        });
+                      },
                     ),
 
                     SizedBox(
@@ -53,7 +69,41 @@ class LoginPage extends StatelessWidget {
                     ),
 
 
-                    ElevatedButton(onPressed: ()
+                   InkWell(
+                     onTap: ()
+                     async {
+                       setState(() {
+                         changebutton = true;
+                       }
+                       );
+                       await Future.delayed(Duration(seconds :1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                     },
+
+
+                     child: AnimatedContainer(
+                       duration: Duration(seconds: 1),
+                       alignment: Alignment.center,
+                       height: 45,
+                       width: changebutton? 45 :120,
+                       child: changebutton? Icon(Icons.done, color: Colors.white,): Text('Login',
+                         style: TextStyle(
+                           color: Colors.white,
+                           fontSize: 17,
+                         ),),
+
+                       decoration: BoxDecoration(
+                        // shape: changebutton? BoxShape.circle : BoxShape.rectangle,
+                         color: Colors.purple,
+                         borderRadius: BorderRadius.circular(changebutton? 45 : 5)
+
+                       ),
+
+                     ),
+                   )
+
+
+                   /* ElevatedButton(onPressed: ()
                     {
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
 
@@ -68,7 +118,7 @@ class LoginPage extends StatelessWidget {
                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
                       ),
 
-                      ),
+                      ),*/
 
 
 
