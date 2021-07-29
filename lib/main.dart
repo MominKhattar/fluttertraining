ce06402546b0e6105f5faf1ea6015872c8e8c843
Momin Khatter <momin.khanzada1999@gmail.com>
2021-07-29 00:19:00 -0700

    "Exploring dark theme"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:fluttertraining/pages/login.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -12,11 +12,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      themeMode: ThemeMode.dark,
+
+      //for changing theme
+
+      themeMode: ThemeMode.light,
+
+      //for changing light theme
+
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+
+      //for changing dark theme data
+
+
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+
+
+      initialRoute: 'login',
+      routes:{
+        '/' : (context)=> HomePage(),
+        '/login' : (context)=>LoginPage(),
+      }
+
     );
   }
 }
+
+
